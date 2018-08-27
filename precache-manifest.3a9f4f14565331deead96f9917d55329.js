f59b7a92241605fe815dda078646014aa7a0b751
idtaysaev <taysaevid@gmail.com>
2018-08-27 17:56:26 +0300

    81e32e2

---
+++ precache-manifest.3a9f4f14565331deead96f9917d55329.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f163f2d14419d914d955bf3d567fa55",
+    "url": "/img/stool.4f163f2d.jpg"
+  },
+  {
+    "revision": "9152d7f1724ed8fbcd2e0c87029f193c",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "568e093078e67fa4f9a62afcc6ef25d4",
+    "url": "/img/clock-big.568e0930.jpg"
+  },
+  {
+    "revision": "acc1ba2f7b2aa570d64f",
+    "url": "/js/chunk-vendors.feabc043.js"
+  },
+  {
+    "revision": "0ceba042c00263e87347",
+    "url": "/js/app.ee0752be.js"
+  },
+  {
+    "revision": "bbd0a358a7bf0544e3e96e24bd2dc67e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b46829807df432e434e4474915b6a0f",
+    "url": "/img/watch.6b468298.jpg"
+  },
+  {
+    "revision": "c451b9b1451d6d5925369d272437ea88",
+    "url": "/img/paper.c451b9b1.jpg"
+  },
+  {
+    "revision": "7cc5af633c3a09837bdff317e5c93d4c",
+    "url": "/img/model.7cc5af63.jpg"
+  },
+  {
+    "revision": "4d0658ef1e71b2897d10db975608c51c",
+    "url": "/img/eyeglass.4d0658ef.jpg"
+  },
+  {
+    "revision": "ebe442fb5b2a16aee5baaf87dc909a65",
+    "url": "/img/clock.ebe442fb.jpg"
+  },
+  {
+    "revision": "7b26d522ff26c879f1969276c93e4c59",
+    "url": "/img/clock.7b26d522.jpg"
+  },
+  {
+    "revision": "0ceba042c00263e87347",
+    "url": "/css/app.ce205515.css"
+  },
+  {
+    "revision": "f362b50c72b151d257ab7f6d7d25ba64",
+    "url": "/CNAME"
+  }
+];
